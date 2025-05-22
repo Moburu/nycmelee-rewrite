e4d908f942c36f63ebe1da52218d1f2cf9030026
Moburu <stevenschiffner3@gmail.com>
2025-05-22 16:09:16 -0400

    fixed react-select types

--- app/players/page.tsx
+++ app/players/page.tsx
@@ -1,28 +1,38 @@
 'use client'
 
+import { Options } from 'react-select'
+import MySelect from '@/components/MySelect'
 import LoadingSpinner from '@/components/LoadingSpinner'
 import { createClient } from '@/utils/supabase/client'
 import { useEffect, useState } from 'react'
 
+type Option = {
+  value: string;
+  label: string;
+}
+
 export default function Page() {
-  const [notes, setNotes] = useState<any[] | null>(null)
+  const [options, setOptions] = useState<Options<Option>>([])
   const [loading, setLoading] = useState<boolean>(true)
   const supabase = createClient()
 
   useEffect(() => {
     const getData = async () => {
-      const { data } = await supabase.from('entrant').select().limit(10)
-      console.log(data)
-      setNotes(data)
+      const { data }: any = await supabase.from('unique_entrants').select()
+      const optionHolder = []
+      for (let i = 0; i < data.length; i++) {
+        optionHolder.push({label: data[i].tag, value: data[i].lowercase_tag})
+      }
+      setOptions(optionHolder)
       setLoading(false)
     }
     getData()
   }, [])
 
   return (
-    <pre>
-        {loading ? <LoadingSpinner /> :
-        JSON.stringify(notes, null, 2)}
-    </pre>
+    <div className="w-full flex flex-col">
+        {loading ? <LoadingSpinner />
+        : <MySelect entrants={options} />}
+    </div>
     )
 }
---
+++ components/MySelect.tsx
@@ -0,0 +1,58 @@
+import Select, { Options } from "react-select";
+
+type Option = {
+    label: string;
+    value: string;
+}
+
+export default function MySelect({ entrants } : { entrants: Options<Option> }): React.JSX.Element {
+
+    const darkStyles = {
+        container: (provided: any) => ({
+          ...provided,
+          backgroundColor: '#222',
+          borderTopRightRadius: '0px',
+          borderBottomRightRadius: '0px'
+        }),
+        control: (provided: any, state: any) => ({
+          ...provided,
+          backgroundColor: '#333',
+          color: '#fff',
+          border: state.isFocused ? '1px solid white' : '1px solid rgb(110, 41, 230)',
+          boxShadow: 'none',
+          '&:hover': {
+          border: '1px solid white',
+          },
+          borderTopRightRadius: '0px',
+          borderBottomRightRadius: '0px'
+        }),
+        menu: (provided: any) => ({
+          ...provided,
+          backgroundColor: '#333',
+          color: '#fff',
+        }),
+        option: (provided: any, state: any) => ({
+          ...provided,
+          backgroundColor: state.isFocused ? '#555' : '#333',
+          color: '#fff',
+        }),
+        singleValue: (provided: any) => ({
+          ...provided,
+          color: '#fff',
+        }),
+        input: (provided: any) => ({
+          ...provided,
+          color: '#fff',
+        })
+    }
+
+    return (
+        <Select
+            className="w-4/5 inline-block text-xl h-10 pl-2"
+            options={entrants}
+            styles={darkStyles}
+            placeholder="Search for someone's tag..."
+            name="playerName"
+        />
+    )
+}
